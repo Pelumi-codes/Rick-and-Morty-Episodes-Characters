06808fc0f781a6c2400cf63b119b7a18a9adae21
Pelumi Codes <alabideborah973@gmail.com  git config --global user.name Pelumi-codes>
2022-03-08 18:03:04 +0100

    Added Episodes components

---
+++ src/components/Episodes/index.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { SingleCharacterCard } from "../Characters/SingleCharacter";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+
+const SingleCharacter = ({ characters }) => {
+  const { id } = useParams();
+
+  const [singleCharacter, setCharacter] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/${id}`
+        );
+        const data = await response.json();
+        setCharacter(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, [id, characters]);
+
+  if (!singleCharacter) return <Box ml="6%">Loading...</Box>;
+  return (
+    <Box mx="30px" mt="2%">
+      <SingleCharacterCard character={singleCharacter} />
+      <Flex flexWrap="wrap">
+        {singleCharacter.episode.map((ep, i) => (
+          <Box
+            p="40px"
+            borderRadius="5px"
+            bg="#FFA500"
+            m="5px"
+            flex="1"
+            whiteSpace="nowrap"
+          >
+            <Text fontSize="19" fontWeight="700">
+              <a href="https://www.youtube.com/watch?v=_uUcMwsR5hg" key={i}>
+                Episode {ep.slice(ep.lastIndexOf("/") + 1)}
+              </a>
+            </Text>
+          </Box>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default SingleCharacter;
